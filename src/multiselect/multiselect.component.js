e8a52293d3f29d843b754927a915e82d90053b88
rende99 <rende99@gmail.com>
2020-05-20 12:04:18 -0400

    added onChangeInput

--- src/multiselect/multiselect.component.js
+++ src/multiselect/multiselect.component.js
@@ -151,10 +151,10 @@ export class Multiselect extends React.Component {
 
   onChange(event) {
     const { onChangeInput } = this.props;
+    onChangeInput(event.target.value);
     this.setState(
       { inputValue: event.target.value }, () => {
-        this.filterOptionsByInput();
-        onChangeInput(event.target.value);
+        this.filterOptionsByInput;
       }
     );
 
